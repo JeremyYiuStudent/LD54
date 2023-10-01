dcfa37a7dd7be9bc2dc3ca2081848b1385425d3b
OriginalPuma <96091587+OriginalPuma@users.noreply.github.com>
2023-09-30 17:17:13 -0700

    health

---
+++ Assets/Health/Health.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Image FillImage;
+    public float CurrentHealth;
+    public float MaxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = 100;
+        MaxHealth = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FillImage.fillAmount = CurrentHealth/100;
+    }
+}
